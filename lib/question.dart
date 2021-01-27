dfa3729764dee5c5e6cd4b19c9a9d3aa343b3f1a
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-27 15:32:17 +0500

    Changes in constructors

--- lib/question.dart
+++ lib/question.dart
@@ -1,9 +1,6 @@
 class Question{
   String QuestionText;
   bool answer;
-  Question(String question, bool answer){
-    this.QuestionText = question;
-    this.answer = answer;
-  }
+  Question(this.QuestionText, this.answer);
 
 }
